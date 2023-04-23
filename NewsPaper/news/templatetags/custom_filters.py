0add7c071ab4132cf742eb4063cae8724b06c7cb
Константин Нехорошев <nekhoroshevkg@russiancrab.ru>
2023-04-23 16:24:37 +1000

    Добавлены шаблоны для новостей, фильтр цензуры, добавил проверку строкого типа

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -5,13 +5,16 @@ register = template.Library()
 
 @register.filter()
 def censor_filter(text: str) -> str:
-    obscene_words = {'редиска', 'свинтус', 'дурак'}
-    text = text.split(' ')
-    for index, word in enumerate(text):
-        word = "".join(symb for symb in word if symb.isalpha())
-        if word in obscene_words:
-            text[index] = word[0] + '*' * (len(word) - 1)
-    return " ".join(text)
+    if type(text) == str:
+        obscene_words = {'редиска', 'свинтус', 'дурак'}
+        text = text.split(' ')
+        for index, word in enumerate(text):
+            word = "".join(symb for symb in word if symb.isalpha())
+            if word in obscene_words:
+                text[index] = word[0] + '*' * (len(word) - 1)
+        return " ".join(text)
+    else:
+        raise ValueError
 
 
 
