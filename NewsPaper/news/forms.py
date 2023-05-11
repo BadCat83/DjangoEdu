761b9434265b779d2c888f108d1ac180196292ee
Bad Cat <badcat@lsit.ru>
2023-05-12 03:09:17 +1000

    Попытка модифицировать форму входа через Google, чтобы автоматически добавлять всех пользователей авторизовавшихся через гугл в группу common. Пока безуспешная

--- NewsPaper/news/forms.py
+++ NewsPaper/news/forms.py
@@ -2,13 +2,14 @@ from django import forms
 from .models import Post
 from .templatetags.utils import censor
 from allauth.account.forms import SignupForm
+from allauth.socialaccount.forms import SignupForm as GoogleSignupForm
 from django.contrib.auth.models import Group
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'text', 'post_author', 'category']
+        fields = ['title', 'text', 'category']
 
     def clean(self):
         cleaned_data = super().clean()
@@ -20,6 +21,15 @@ class PostForm(forms.ModelForm):
 class BaseRegisterForm(SignupForm):
     def save(self, request):
         user = super(BaseRegisterForm, self).save(request)
+        basic_group = Group.objects.get(name='common')
+        basic_group.user_set.add(user)
+        return user
+
+
+class GoogleRegisterForm(GoogleSignupForm):
+    def save(self, request):
+        print("in form!")
+        user = super(GoogleRegisterForm, self).save(request)
         basic_group = Group.objects.get(name='common')
         basic_group.user_set.add(user)
         return user
