13d17ab4097f49ac5970f61107341689db0704e2
Bad Cat <badcat@lsit.ru>
2023-05-15 00:48:34 +1000

    Не знаю как получить category

--- NewsPaper/news/sygnals.py
+++ NewsPaper/news/sygnals.py
@@ -8,29 +8,27 @@ from .models import Post
 @receiver(post_save, sender=Post)
 def send_email(sender, instance, created, **kwargs) -> None:
     if created:
-        print(instance)
-        print(sender)
-    else:
-        pass
-    # title = request.POST['title']
-    # text = request.POST['text']
-    # category = request.POST['category']
-    # users = User.objects.filter(category__id=category)
-    # if users:
-    #     html_content = render_to_string(
-    #         'post_created.html',
-    #         {
-    #             'post_type': post_type,
-    #             'title': title,
-    #             'text': text,
-    #         }
-    #     )
-    #     for user in users:
-    #         msg = EmailMultiAlternatives(
-    #             subject=f"{title}",
-    #             body=f"Здравствуй, {user.username}. Новая статья в твоём любимом разделе!",
-    #             from_email=DEFAULT_FROM_EMAIL,
-    #             to=[user.email],
-    #         )
-    #         msg.attach_alternative(html_content, "text/html")
-    #         msg.send()
+        title = instance.title
+        text = instance.text
+        category = instance.category
+        print(category)
+
+        # users = User.objects.filter(category__id=category)
+        # if users:
+        #     html_content = render_to_string(
+        #         'post_created.html',
+        #         {
+        #             'post_type': post_type,
+        #             'title': title,
+        #             'text': text,
+        #         }
+        #     )
+        #     for user in users:
+        #         msg = EmailMultiAlternatives(
+        #             subject=f"{title}",
+        #             body=f"Здравствуй, {user.username}. Новая статья в твоём любимом разделе!",
+        #             from_email=DEFAULT_FROM_EMAIL,
+        #             to=[user.email],
+        #         )
+        #         msg.attach_alternative(html_content, "text/html")
+        #         msg.send()
