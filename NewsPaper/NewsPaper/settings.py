d26860a901a48f27dc42b38556a7ea1f3ca021e9
Константин Нехорошев <nekhoroshevkg@russiancrab.ru>
2023-05-27 12:57:10 +1000

    Добавлены celery и redis

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -147,3 +147,9 @@ ACCOUNT_FORMS = {'signup': 'news.forms.BaseRegisterForm'}
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 SITE_ID = 1
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
