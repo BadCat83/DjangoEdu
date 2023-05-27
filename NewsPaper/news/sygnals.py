d1262ec7a81f9506f4fff0b22ad93ea335222d6d
Константин Нехорошев <nekhoroshevkg@russiancrab.ru>
2023-05-27 13:50:56 +1000

    Отправляем почту через celery

--- NewsPaper/news/sygnals.py
+++ NewsPaper/news/sygnals.py
@@ -7,6 +7,9 @@ from allauth.account.signals import user_signed_up
 from .models import PostCategory
 from NewsPaper.settings import DEFAULT_FROM_EMAIL, SITE_URL
 
+from .tasks import send_email_through_celery
+
+
 @receiver(user_signed_up)
 def user_signed_up_(request, user, **kwargs):
     username = user.username
@@ -29,30 +32,4 @@ def user_signed_up_(request, user, **kwargs):
 @receiver(m2m_changed, sender=PostCategory)
 def send_email(sender, instance, **kwargs) -> None:
     if kwargs['action'] == 'post_add':
-        title = instance.title
-        text = instance.text
-        pk = instance.pk
-        post_type, link = ('новость', 'news') if instance.post_type=='NW' else ('статья', 'article')
-        categories = instance.category.all()
-        users : list[str] = []
-        for category in categories:
-            users += category.subscribers.all()
-        if users:
-            html_content = render_to_string(
-                'post_created.html',
-                {
-                    'link': f'{SITE_URL}/{link}/{pk}',
-                    'post_type': post_type,
-                    'title': title,
-                    'text': text,
-                }
-            )
-            for user in users:
-                msg = EmailMultiAlternatives(
-                    subject=f"{title}",
-                    body=f"Здравствуй, {user.username}. Новая {post_type} в твоём любимом разделе!",
-                    from_email=DEFAULT_FROM_EMAIL,
-                    to=[user.email],
-                )
-                msg.attach_alternative(html_content, "text/html")
-                msg.send()
+        send_email_through_celery(instance)
