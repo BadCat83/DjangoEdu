0e1927ba9b03888850995e5bcc7ab4535d3c82f7
Константин Нехорошев <nekhoroshevkg@russiancrab.ru>
2023-05-29 21:25:34 +1000

    Переделал отправку оповещений о посте, добавил еженедельную рассылку

--- NewsPaper/news/tasks.py
+++ NewsPaper/news/tasks.py
@@ -1,18 +1,20 @@
+import os
+from datetime import datetime, timedelta
+
 from celery import shared_task
-from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
-from NewsPaper.settings import DEFAULT_FROM_EMAIL, SITE_URL
+from NewsPaper.settings import SITE_URL
+
+from .models import Post, Category
 
 
 @shared_task
-def send_email_through_celery(instance) -> None:
-    title = instance.title
-    text = instance.text
-    pk = instance.pk
-    post_type, link = ('новость', 'news') if instance.post_type == 'NW' else ('статья', 'article')
-    categories = instance.category.all()
+def send_email_through_celery(pk) -> None:
+    post = Post.objects.get(pk=pk)
+    post_type, link = ('новость', 'news') if post.post_type == 'NW' else ('статья', 'article')
+    categories = post.category.all()
     users: list[str] = []
     for category in categories:
         users += category.subscribers.all()
@@ -20,18 +22,49 @@ def send_email_through_celery(instance) -> None:
         html_content = render_to_string(
             'post_created.html',
             {
-                'link': f'{SITE_URL}/{link}/{pk}',
+                'link': f'{SITE_URL}/{link}/{post.pk}',
                 'post_type': post_type,
-                'title': title,
-                'text': text,
+                'title': post.title,
+                'text': post.text,
             }
         )
         for user in users:
             msg = EmailMultiAlternatives(
-                subject=f"{title}",
+                subject=f"{post.title}",
                 body=f"Здравствуй, {user.username}. Новая {post_type} в твоём любимом разделе!",
-                from_email=DEFAULT_FROM_EMAIL,
+                from_email=os.getenv('EMAIL'),
                 to=[user.email],
+            )
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+
+@shared_task
+def send_weekly_news():
+    users = []
+    last_week = datetime.now() - timedelta(days=7)
+    posts = Post.objects.filter(creation_time__gte=last_week)
+    categories = set(posts.values_list('category__category', flat=True))
+    for category in categories:
+        users += Category.objects.filter(category=category).values_list(
+            'subscribers__username',
+            'subscribers__email'
+        )
+    html_content = render_to_string(
+        'posts_last_week.html',
+        {
+            'link': f'{SITE_URL}',
+            'posts': posts,
+        }
+    )
+    for user in set(users):
+        user_name, user_email = user
+        if user_name:
+            msg = EmailMultiAlternatives(
+                subject=f"Здравствуй, {user_name}.",
+                body='',
+                from_email=os.getenv('EMAIL'),
+                to=[user_email],
             )
             msg.attach_alternative(html_content, "text/html")
             msg.send()
