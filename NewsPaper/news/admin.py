d5a10eb91ed8306bf024b40797f901b090189162
Bad Cat <badcat@lsit.ru>
2023-06-21 22:05:48 +1000

    Модификация админки

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+def nullfy_rate(modeladmin, request, queryset):
+    queryset.update(rate=0)
+
+nullfy_rate.short_description = 'Обнулить рейтинг'
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'post_author', 'post_type', 'rate', 'creation_time')
+    list_filter = ('post_author', 'post_type', 'creation_time')
+    search_fields = ('title', 'post_author', 'post_type', 'creation_time')
+    actions = [nullfy_rate]
+
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
